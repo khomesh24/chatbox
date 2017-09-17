10f439b65553a63403df5c8c3aed60154b5fc9af
khomesh24 <khomeshthakre24@gmail.com>
2017-09-18 01:43:41 +0530

    both client and server can send message and recieve message

--- client.py
+++ client.py
@@ -7,7 +7,16 @@ def start():
     cli1 = socket.socket()
     cli1.connect((host,port))
 
-    print("client started")
+    msg = input("->")
+
+    while msg != 'q':
+        cli1.send(msg.encode());
+        ser_msg = cli1.recv(1024).decode()
+        print("form server: "+str(ser_msg))
+        msg = input("->")
+
+    cli1.close()
+
 
 if __name__ == '__main__':
     start()
